8251fbb7c0ff6f2b8ce688dcbe5c4de80a349314
Krutika26 <60579383+Krutika26@users.noreply.github.com>
2023-08-16 15:42:14 +0530

    Update UseCase.java

--- opportunity_management_system/src/main/java/com/atc/opportunity_management_system/entity/UseCase.java
+++ opportunity_management_system/src/main/java/com/atc/opportunity_management_system/entity/UseCase.java
@@ -11,7 +11,7 @@ import lombok.Data;
 @Entity
 @Table(name="useCase")
 @Data
-public class User {
+public class UseCase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
