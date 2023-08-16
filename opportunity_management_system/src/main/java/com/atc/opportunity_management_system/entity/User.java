3c6ee3a4c412d25529cd347da7a0643d23488703
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-16 17:18:46 +0530

    added common db

--- opportunity_management_system/src/main/java/com/atc/opportunity_management_system/entity/User.java
+++ opportunity_management_system/src/main/java/com/atc/opportunity_management_system/entity/User.java
@@ -17,9 +17,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(name="username", nullable = false, unique = true)
+    private String username;
 
     @Column(name="firstName", nullable = false)
     private String firstName;
+    
 
     @Column(name="lastName", nullable = false)
     private String lastName;
