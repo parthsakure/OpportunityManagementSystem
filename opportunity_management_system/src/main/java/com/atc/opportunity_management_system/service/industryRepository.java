ece772e535e829dd940082d8297a5fd847640432
amanlad97 <76096700+amanlad97@users.noreply.github.com>
2023-08-16 17:08:30 +0530

    industry repository

--- opportunity_management_system/src/main/java/com/atc/opportunity_management_system/service/industryRepository.java
+++ opportunity_management_system/src/main/java/com/atc/opportunity_management_system/service/industryRepository.java
@@ -1,1 +1,13 @@
 package com.atc.opportunity_management_system.service;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.atc.opportunity_management_system.entity.industry;
+
+
+@RepositoryRestResource(path="industry")
+public interface industryRepository extends JpaRepository <industry ,Long>{
+
+}
