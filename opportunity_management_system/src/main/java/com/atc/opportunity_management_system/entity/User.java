cab0a755bcae5224c75afcf911f4be08631c8053
amanlad97 <76096700+amanlad97@users.noreply.github.com>
2023-08-17 12:27:13 +0530

    user validation

--- opportunity_management_system/src/main/java/com/atc/opportunity_management_system/entity/User.java
+++ opportunity_management_system/src/main/java/com/atc/opportunity_management_system/entity/User.java
@@ -2,6 +2,7 @@ package com.atc.opportunity_management_system.entity;
 
 import java.util.List;
 
+
 import jakarta.persistence.CascadeType;
 
 import jakarta.persistence.Column;
@@ -13,6 +14,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Entity
@@ -23,26 +27,29 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     
-    @Column(name="username", nullable = false, unique = true)
+    @Column(name="username", nullable = false, unique = true,length = 30)
+    @NotBlank(message = "username is mandatory")
     private String username;
 
-    @Column(name="firstName", nullable = false)
+    @Column(name="firstName", nullable = false,length=20)
     private String firstName;
     
-    @Column(name="lastName", nullable = false)
+    @Column(name="lastName", nullable = false, length =30)
     private String lastName;
 
+    @Email(message = "email is mandatory")
     @Column(name="email", nullable = false)
     private String email;
 
-    @Column(name="contactNo", nullable = false)
+    @Column(name="contactNo", nullable = false ,length = 15)
+    @Pattern(regexp="^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]\\d{3}[\\s.-]\\d{4}$")
     private String contactNo;
 
-    @Column(name="bbdBucks", nullable = false)
-    private int bbdBucks;
+    @Column(name="bbdBucks", nullable = false, length = 7)
+    private int bbdBucks=0;
     
-    @Column(name="active", nullable = false)
-    private Boolean active;
+    @Column(name="active")
+    private Boolean active=true;
 
     @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
     @JoinColumn(name="roleId")
