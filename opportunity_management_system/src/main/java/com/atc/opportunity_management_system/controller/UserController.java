dd94bafeb9ae9eeaffb749ecac2767f7b8cda92a
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-24 11:51:03 +0530

    user service with parameter is added

--- opportunity_management_system/src/main/java/com/atc/opportunity_management_system/controller/UserController.java
+++ opportunity_management_system/src/main/java/com/atc/opportunity_management_system/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.atc.opportunity_management_system.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.atc.opportunity_management_system.entity.ErrorMessage;
 import com.atc.opportunity_management_system.entity.User;
 import com.atc.opportunity_management_system.service.UserService;
 
@@ -23,21 +27,26 @@ public class UserController {
     private UserService userService;
 
     @PutMapping("/{userId}")
-    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody @Valid User updateUser) {
+    public ResponseEntity<Object> updateUser(@PathVariable Long userId, @RequestBody @Valid User updateUser) {
         User user = userService.updateUser(userId, updateUser);
         if (user != null) {
             return ResponseEntity.ok(user);
         }
-        return ResponseEntity.notFound().build();
+        return new ResponseEntity<Object>(new ErrorMessage("User not found. Id: "+userId,HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<User> deleteUser(@PathVariable  Long userId) {
+    public ResponseEntity<Object> deleteUser(@PathVariable  Long userId) {
         User user = userService.deleteUser(userId);
         if (user != null) {
             return ResponseEntity.ok(user);
         }
-        return ResponseEntity.notFound().build();
+        return new ResponseEntity<Object>(new ErrorMessage("User not found. Id: "+userId,HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> getAllUsers(@RequestParam(required = false) boolean active){
+        return userService.getAllUsers(active);
     }
 
 
