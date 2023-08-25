bac5fb48ed1308b84f1ae82cf1882ced8fc0b955
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-25 16:21:36 +0530

    Delivery Model service updated

--- opportunity_management_system/src/main/java/com/atc/opportunity_management_system/controller/DeliveryModelController.java
+++ opportunity_management_system/src/main/java/com/atc/opportunity_management_system/controller/DeliveryModelController.java
@@ -3,8 +3,11 @@ package com.atc.opportunity_management_system.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,11 +28,21 @@ public class DeliveryModelController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> getDeliveryModel(@PathVariable int id){
+    public ResponseEntity<Object> getDeliveryModel(@PathVariable Long id){
         DeliveryModel deliveryModel = deliveryModelService.getDeliveryModel(id);
         if(deliveryModel == null){
             return new ResponseEntity<>(new ErrorMessage("Delivery Model not found id: "+ id, HttpStatus.NOT_FOUND.value()),HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok().body(deliveryModel);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Object> addDeliveryModel(@RequestBody DeliveryModel deliveryModel){
+        return deliveryModelService.addDeliveryModel(deliveryModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteDeliveryModel(@PathVariable Long id){
+        return deliveryModelService.deleteDeliveryModel(id);
     }
 }
