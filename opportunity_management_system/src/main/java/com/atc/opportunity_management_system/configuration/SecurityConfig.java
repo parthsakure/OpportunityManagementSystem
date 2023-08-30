88cf013f5c28f1f20423fdbfc15eaecb037aafd1
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-30 16:52:10 +0530

    imports

--- opportunity_management_system/src/main/java/com/atc/opportunity_management_system/configuration/SecurityConfig.java
+++ opportunity_management_system/src/main/java/com/atc/opportunity_management_system/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.atc.opportunity_management_system.configuration;
 
 import org.springframework.security.core.userdetails.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @EnableWebSecurity
