a1fb996528ea699e969d2c067bd77c738c171c1f
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-30 17:18:39 +0530

    filter added

--- opportunity_management_system/src/main/java/com/atc/opportunity_management_system/configuration/SecurityConfig.java
+++ opportunity_management_system/src/main/java/com/atc/opportunity_management_system/configuration/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -59,6 +60,7 @@ public class SecurityConfig implements WebMvcConfigurer {
         .logout(cust->cust
             .logoutUrl("/logout")
         )
+        .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
         .csrf(cust->cust.disable())
         .build();
     }
